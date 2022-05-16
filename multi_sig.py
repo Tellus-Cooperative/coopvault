176d2538826c3e217d268c787c57914c8672f3ad
alexanderkoh <alex@telluscoop.com>
2022-05-16 05:16:23 -0400

    create_coop()

--- multi_sig.py
+++ multi_sig.py
@@ -2,47 +2,51 @@ from stellar_sdk import Asset, Keypair, Network, Server, Signer, TransactionBuil
 import json
 import requests
 
-keypair = Keypair.random()
 
-print("Keypair generated successfully\n")
+def create_coop():
+    keypair = Keypair.random()
 
-print("Public: ", keypair.public_key)
-print("Secret: ", keypair.secret)
+    print("Keypair generated successfully\n")
 
-url = "https://friendbot.stellar.org"
-response = requests.get(url, params={"addr": keypair.public_key})
-print("\nServer:\n", response)
+    print("Public: ", keypair.public_key)
+    print("Secret: ", keypair.secret)
 
-server = Server(horizon_url="https://horizon-testnet.stellar.org")
-root_keypair = Keypair.from_secret(keypair.secret)
+    url = "https://friendbot.stellar.org"
+    response = requests.get(url, params={"addr": keypair.public_key})
+    print("\nServer:\n", response)
 
-root_account = server.load_account(account_id=root_keypair.public_key)
+    server = Server(horizon_url="https://horizon-testnet.stellar.org")
+    root_keypair = Keypair.from_secret(keypair.secret)
 
-"""secondary_public_key = input("Enter Public Key: ").upper()
-"""
-"""secondary_keypair = Keypair.from_secret(
-"SAV2G4KFDGJJS7DNXTQDKVBZ2QKLBLU7UFIB4FZXZMKSDS53JIIEHGPC")"""
+    root_account = server.load_account(account_id=root_keypair.public_key)
 
-secondary_signer = Signer.ed25519_public_key(account_id=input("Enter Public Key: ").upper(), weight=1
-)
+    """secondary_public_key = input("Enter Public Key: ").upper()
+    """
+    """secondary_keypair = Keypair.from_secret(
+    "SAV2G4KFDGJJS7DNXTQDKVBZ2QKLBLU7UFIB4FZXZMKSDS53JIIEHGPC")"""
 
-transaction = (
-    TransactionBuilder(
-        source_account=root_account,
-        network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE,
-        base_fee=100,
+    secondary_signer = Signer.ed25519_public_key(account_id=input("Enter Public Key: ").upper(), weight=1
+                                                 )
+
+    transaction = (
+        TransactionBuilder(
+            source_account=root_account,
+            network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE,
+            base_fee=100,
         )
-        .append_set_options_op(
+            .append_set_options_op(
             master_weight=1,
             low_threshold=1,
             med_threshold=2,
             high_threshold=2,
             signer=secondary_signer,
         )
-        .set_timeout(30)
-        .build()
-)
+            .set_timeout(30)
+            .build()
+    )
 
-transaction.sign(root_keypair)
-response = server.submit_transaction(transaction)
-print(json.dumps(response, indent=4))
+    transaction.sign(root_keypair)
+    response = server.submit_transaction(transaction)
+    print(json.dumps(response, indent=4))
+
+create_coop()
